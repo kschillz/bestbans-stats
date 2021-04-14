8652ad305734431ff8bcd1937ab50be1317e83f2
Kyle Schiller <kgschiller@gmail.com>
2021-04-13 21:41:22 -0400

    don't write files if lolalytics has no data

--- main.ts
+++ main.ts
@@ -9,16 +9,24 @@ async function getLatestPatch() {
 }
 
 (async () => {
+    var lolalyticsHasData = true;
     const patch = await getLatestPatch();
     const path = `static/${patch}`;
     fs.mkdirSync(path, { recursive: true });
     for (const tier in Tier) {
         let bestBans_api = await getStats(Tier[tier], patch);
+        if (bestBans_api.champions.length === 0) {
+            lolalyticsHasData = false;
+            console.log('lolalytics has no data');
+            continue;
+        }
         fs.writeFileSync(`${path}/${tier.toLowerCase()}.json`, JSON.stringify(bestBans_api));
         console.log(tier);
     }
-    fs.writeFileSync('static/meta.json', JSON.stringify({
-        last_updated: new Date(),
-        latest_patch: patch,
-    }))
+    if (lolalyticsHasData) {
+        fs.writeFileSync('static/meta.json', JSON.stringify({
+            last_updated: new Date(),
+            latest_patch: patch,
+        }));
+    }
 })();
